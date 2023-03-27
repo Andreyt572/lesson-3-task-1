4e50a74142766675f12fb1ee41d8d1b978b4e41f
Andrey Tolokonnikov <andrey572@mail.ru>
2023-03-27 18:09:16 +0300

    seems to have corrected Philip's remark

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price, int bonusPerMile) {
+    public int calculate(int price) {
         int result;
-        if (price > bonusPerMile) {
-            result = (price / bonusPerMile);
-        } else {
-            result = 0;
-        }
+        result = price / 20;
         return result;
     }
-
 }
